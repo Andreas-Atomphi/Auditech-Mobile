99023d9ae2e2b03a315b672cb1ec1abb7f3070d8
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-21 00:35:09 -0300

    [FEAT] TelaExemplo adicionada.

---
+++ lib/telas/TelaExemplo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:AuditechMobile/main.dart';
+
+class TelaExemplo extends State<StatefulDisplay> {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+                child: Text(
+              "Exemplo",
+              textAlign: TextAlign.center,
+            )),
+          ),
+          body: Stack(
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Esta tela é um exemplo feita para ser apagada no final do projeto (é necessário deixá-la aqui caso não tenha mais nenhuma tela pronta)",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
