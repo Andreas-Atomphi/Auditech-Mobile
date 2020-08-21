2314e91aaf0e835174849e11d3a1d8efa1a54d0b
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-21 18:24:22 -0300

    [FEAT] Remodelado para navegação entre telas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:AuditechMobile/telas/TelaExemplo.dart';
+import 'package:AuditechMobile/telas/Telas.dart';
 
-void main() {
-  runApp(StatefulDisplay());
+void main() => setAppup();
+
+Map<String, Widget Function(BuildContext context)> routes = {
+  "exemploPrincipal": (context) => TelaExemplo(),
+  "exemplo2": (context) => TelaExemplo2()
+};
+
+MaterialApp aplicativo = MaterialApp(
+  initialRoute: "principal",
+  routes: routes,
+  home: TelaExemplo(),
+);
+
+void setAppup() {
+  runApp(aplicativo);
 }
-
-//Renderiza as telas
-class StatefulDisplay extends StatefulWidget {
-  @override
-  State<StatefulDisplay> createState() {
-    return TelaExemplo();
-  }
-} // StatefulDisplay
