de469f108ac471750f496cf4f99343eb4ba55904
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-25 01:23:26 -0300

    [FEAT] Removidos os imports inúteis

--- lib/telas/TelaLogin.dart
+++ lib/telas/TelaLogin.dart
@@ -1,4 +1,3 @@
-import 'package:AuditechMobile/telas/CustomComponents/ButtonLogin.dart';
 import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/CustomComponents/TelaLogin/components.dart';
 
