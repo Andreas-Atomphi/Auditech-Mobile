421ea691b229c26af1d7d5dfc09dfb0794dad016
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-25 01:18:19 -0300

    [FEAT] Melhorando o sistema de posicionamento.

--- lib/telas/CustomComponents/TelaLogin/TextFieldLogin.dart
+++ lib/telas/CustomComponents/TelaLogin/TextFieldLogin.dart
@@ -3,19 +3,30 @@ import 'package:flutter/material.dart';
 class TextFieldLogin extends StatelessWidget {
   final String dica;
   final bool obscure;
+  final Alignment alinhar;
+  final EdgeInsets margens;
 
-  TextFieldLogin(this.dica, this.obscure);
+  TextFieldLogin(this.dica, this.obscure, this.alinhar, this.margens);
 
   Widget build(BuildContext context) {
-    return TextField(
-      style: TextStyle(color: Colors.black),
-      decoration: InputDecoration(
-        labelStyle: TextStyle(color: Colors.lightBlue),
-        hintText: dica,
-        isDense: true,
-        contentPadding: EdgeInsets.all(8),
+    return Center(
+      child: Container(
+        child: TextField(
+          style: TextStyle(color: Colors.black),
+          decoration: InputDecoration(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(2)),
+            fillColor: Colors.white,
+            filled: true,
+            hintText: dica,
+            hintStyle: TextStyle(color: Colors.grey),
+            isDense: true,
+            contentPadding: EdgeInsets.all(8),
+          ),
+          obscureText: obscure,
+        ),
+        margin: margens,
+        width: (MediaQuery.of(context).size.width / 100) * 65,
       ),
-      obscureText: obscure,
     );
   }
 }
