f5a9eaca35045a33bd4dab66860594289f8aeaa9
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-25 01:12:39 -0300

    [FEAT] Removidos os imports inúteis. [FEAT] Mudança de cores para mais amigáveis. [FEAT] Reorganizada a estrutura de TelaLogin

--- lib/telas/TelaLogin.dart
+++ lib/telas/TelaLogin.dart
@@ -1,4 +1,4 @@
-import 'package:AuditechMobile/main.dart';
+import 'package:AuditechMobile/telas/CustomComponents/ButtonLogin.dart';
 import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/CustomComponents/TelaLogin/components.dart';
 
@@ -6,17 +6,35 @@ class _LoginState extends State {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
           primaryColor: Colors.lightBlue,
           accentColor: Colors.cyan),
       home: Scaffold(
-        body: Stack(children: [
-          Align(
-            child: FormLogin(),
-            alignment: Alignment(0, 0),
-          ),
-        ]),
-      ),
+          backgroundColor: Color.fromARGB(255, 0, 70, 100),
+          body: Stack(
+            children: [
+              FormLogin(
+                [
+                  TextFieldLogin("Email", false, Alignment(0, 0),
+                      EdgeInsets.only(top: 0, bottom: 60.0)),
+                  TextFieldLogin("Senha", true, Alignment(0, 0),
+                      EdgeInsets.only(top: 60.0, bottom: 0)),
+                  ButtonLogin("Entrar", () {}, Alignment(0, 0),
+                      EdgeInsets.only(top: 200, bottom: 0), false),
+                  ButtonLogin("Registrar-se", () {}, Alignment(0, 0),
+                      EdgeInsets.only(top: 300.0), true),
+                  Container(
+                    child: Text(
+                      "developed by: H.A.W.Ga.M",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    alignment: Alignment(-1, 1),
+                  )
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
