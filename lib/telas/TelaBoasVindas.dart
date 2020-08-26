f1fbbe9da0a3c05e0e837fac36832eb4aaecffa1
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-26 01:12:07 -0300

    [FEAT] Adicionado Drawer. [FEAT] Remoção dos botões feita.

--- lib/telas/TelaBoasVindas.dart
+++ lib/telas/TelaBoasVindas.dart
@@ -1,13 +1,14 @@
 import 'package:AuditechMobile/main.dart';
 import 'package:flutter/material.dart';
 import 'CustomComponents/TelaBoasVindas/components.dart';
+import 'CustomComponents/TelaBoasVindas/Conjunct.dart';
 
 class _TelaBoasVindasState extends State {
   @override
   Widget build(BuildContext context) {
     List<Widget> components = [
       Spacer(
-        flex: 4,
+        flex: 1,
       ),
       InstructAndGreetings(
           "Aqui teremos mensagens de boas-vindas e instruções iniciais"),
@@ -15,12 +16,10 @@ class _TelaBoasVindasState extends State {
         flex: 1,
       ),
     ];
-    for (int i = 0; i < bwConjunct.length; i++) {
-      components.add(bwConjunct[i]);
-    }
     return MaterialApp(
       home: Scaffold(
         backgroundColor: backgroundColor,
+        drawer: DrawerWelcome(),
         appBar: AppBar(
           title: Text("Bem-vindo [Usuário]"),
           centerTitle: true,
