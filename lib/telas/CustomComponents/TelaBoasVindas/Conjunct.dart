f49632516f63c5c2f56e2bea979f2fec76e76cb9
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-26 01:13:18 -0300

    [FEAT] Renomeado de BWConjunct para Conjunct. [FEAT] Removida a lista de botões e adicionada a lita de Maps para ListTile.

---
+++ lib/telas/CustomComponents/TelaBoasVindas/Conjunct.dart
@@ -0,0 +1,5 @@
+List<Map<String, dynamic>> ltwConjunct = [
+  {"texto": "Treinamentos", "metodo": () => null},
+  {"texto": "Estatísticas", "metodo": () => null},
+  {"texto": "Sobre DPAC", "metodo": () => null},
+];
