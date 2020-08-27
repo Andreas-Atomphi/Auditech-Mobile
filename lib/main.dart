9eb42afceb06815c0955c258f2b0cd20b6f1668e
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-27 20:54:16 -0300

    [FEAT] Alterado o esquema de cores do tema. [FEAT] Alterada a primeira tela para TelaTreinamento para testes.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/Telas.dart';
 import 'package:AuditechMobile/telas/routes.dart';
 
-Color backgroundColor = Color.fromARGB(255, 0, 70, 100);
+Color backgroundColor = Color.fromARGB(255, 0, 90, 100);
 Brightness theme = Brightness.light;
-Color primary = Colors.lightBlue;
-Color accent = Colors.cyan;
+Color primary = Color.fromARGB(255, 0, 200, 190);
+Color accent = Color.fromARGB(255, 0, 150, 160);
 void main() => setAppup();
 MaterialApp aplicativo = MaterialApp(
-  initialRoute: "principal",
+  initialRoute: "treinamento-1",
   routes: routes,
   home: TelaLogin(),
   theme: ThemeData(
