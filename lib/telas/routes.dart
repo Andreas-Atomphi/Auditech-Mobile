b59250183ebfe20b924151e5b24aac173397fd52
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-27 20:55:07 -0300

    [FEAT] Removidos os imports desnecessários. [FEAT] Adicionada uma nova route para TelaTreinamento.

--- lib/telas/routes.dart
+++ lib/telas/routes.dart
@@ -1,7 +1,10 @@
+import 'package:AuditechMobile/telas/TelaTreinamento.dart';
+
 import 'Telas.dart';
 import 'package:flutter/material.dart';
 
 Map<String, Widget Function(BuildContext context)> routes = {
   "principal": (context) => TelaLogin(),
   "boas-vindas": (context) => TelaBoasVindas(),
+  "treinamento-1": (context) => TelaTreinamento(),
 };
