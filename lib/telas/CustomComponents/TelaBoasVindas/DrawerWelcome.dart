40787f9a36919f5951875d0bb503e886323405be
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-27 21:01:00 -0300

    [FEAT] Adicionada nova cor através do componente Ink. [FEAT] Adicionada imagem decorativa ao drawer.

--- lib/telas/CustomComponents/TelaBoasVindas/DrawerWelcome.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/DrawerWelcome.dart
@@ -5,18 +5,26 @@ import 'ListTileWelcome.dart';
 class DrawerWelcome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: <Widget>[
-          DrawerHeader(
-            child: Text('Logo da Auditech/Hawgam'),
-            decoration: BoxDecoration(
-              color: Colors.blue,
+      child: Ink(
+        color: Color.fromARGB(255, 0, 150, 140),
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/Logo_02.jpg',
+                    ),
+                    fit: BoxFit.cover),
+              ),
+              child: null,
             ),
-          ),
-          ...ltwConjunct.map((e) {
-            return ListTileWelcome(e["texto"], () => e["aoTocar"]);
-          }).toList(),
-        ],
+            ...ltwConjunct.map((e) {
+              return ListTileWelcome(e["texto"], () => e["aoTocar"]);
+            }).toList(),
+          ],
+        ),
       ),
     );
   }
