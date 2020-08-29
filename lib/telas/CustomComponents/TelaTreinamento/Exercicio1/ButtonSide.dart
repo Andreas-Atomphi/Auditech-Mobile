3c388fdd7e25c37e6f53017aca0ddd189617a3d5
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 06:24:03 -0300

    [FEAT] Criado o componente ButtonSide.

---
+++ lib/telas/CustomComponents/TelaTreinamento/Exercicio1/ButtonSide.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SideButton extends StatelessWidget {
+  final String texto;
+  final Map<String, Color> cor;
+
+  SideButton(
+    this.texto, {
+    this.cor = const <String, Color>{
+      "texto": Colors.white,
+      "fundo": Colors.orange,
+    },
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text(texto),
+        onPressed: () {},
+        color: cor["fundo"],
+        textColor: cor["texto"],
+      ),
+      height: (MediaQuery.of(context).size.height / 100) * 50,
+      width: (MediaQuery.of(context).size.width / 100) * 45,
+    );
+  }
+}
