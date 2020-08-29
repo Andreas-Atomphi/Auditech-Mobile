872e92cb0ffb9a5261e37e0040b6857c59494290
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 09:31:21 -0300

    [FEAT] Criada a placeholder da TelaEstatistica.(WIP)

---
+++ lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaEstatísticas.dart
@@ -0,0 +1,15 @@
+import 'package:AuditechMobile/telas/CustomComponents/Global/globalComponents.dart';
+import 'package:flutter/material.dart';
+
+import '../components.dart';
+
+class AbaEstatisticas extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InstructAndGreetings(
+            "Aqui teremos mensagens de boas-vindas e instruções iniciais"),
+      ],
+    );
+  }
+}
