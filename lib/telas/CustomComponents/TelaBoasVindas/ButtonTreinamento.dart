a6962b11aa06c339d5682891ac60ce10261ca015
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 09:28:55 -0300

    [FEAT] Componente criado.

---
+++ lib/telas/CustomComponents/TelaBoasVindas/ButtonTreinamento.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ButtonTreinamento extends StatelessWidget {
+  final String texto;
+  final void Function() aoPressionar;
+
+  ButtonTreinamento(
+    this.texto,
+    this.aoPressionar,
+  );
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        onPressed: aoPressionar,
+        child: Text(
+          texto,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+            fontSize: 17,
+          ),
+        ),
+        color: Colors.orange,
+      ),
+      width: 130,
+      height: 130,
+    );
+  }
+}
