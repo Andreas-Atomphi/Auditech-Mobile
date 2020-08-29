f5b25e5cad4a3701077d7011fb75f4636d52176a
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 06:16:31 -0300

    [FEAT] Mudada a tela de aviso de treinamento. [FEAT] Adicionado o import de globalComponents.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/Telas.dart';
 import 'package:AuditechMobile/telas/routes.dart';
+import 'package:AuditechMobile/telas/CustomComponents/Global/globalComponents.dart';
 
 Color backgroundColor = Color.fromARGB(255, 0, 90, 100);
 Brightness theme = Brightness.light;
@@ -8,7 +9,7 @@ Color primary = Color.fromARGB(255, 0, 200, 190);
 Color accent = Color.fromARGB(255, 0, 150, 160);
 void main() => setAppup();
 MaterialApp aplicativo = MaterialApp(
-  initialRoute: "treinamento-1",
+  initialRoute: "treinamento-aviso",
   routes: routes,
   home: TelaLogin(),
   theme: ThemeData(
