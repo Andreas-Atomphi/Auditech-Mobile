38a2380fea2f841c191c95ff932a2da5fb86d67b
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 06:17:29 -0300

    [FEAT] Removido o import desnecessário. [FEAT] Renomeada a tela de aviso de treinamento. [FEAT] Adicionada a route de TelaTreinamento.

--- lib/telas/routes.dart
+++ lib/telas/routes.dart
@@ -1,10 +1,9 @@
-import 'package:AuditechMobile/telas/TelaTreinamento.dart';
-
 import 'Telas.dart';
 import 'package:flutter/material.dart';
 
 Map<String, Widget Function(BuildContext context)> routes = {
   "principal": (context) => TelaLogin(),
   "boas-vindas": (context) => TelaBoasVindas(),
-  "treinamento-1": (context) => TelaTreinamento(),
+  "treinamento-aviso": (context) => TelaInstrucoesTreinamento(),
+  "treinamento": (context) => TelaTreinamento(),
 };
