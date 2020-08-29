b303eda125f3cc82e745ba67cea561d9f8950866
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 06:20:58 -0300

    [FEAT] Adicionada a opção de ativar o botão de voltar.

--- lib/telas/CustomComponents/Global/CAppBar.dart
+++ lib/telas/CustomComponents/Global/CAppBar.dart
@@ -4,11 +4,13 @@ import 'package:AuditechMobile/main.dart';
 class CAppBar extends AppBar {
   final String texto;
   final bool centralizar;
+  final bool backButton;
 
-  CAppBar(this.texto, [this.centralizar = true])
+  CAppBar(this.texto, {this.centralizar = true, this.backButton = false})
       : super(
           title: Text(texto),
           centerTitle: centralizar,
           backgroundColor: primary,
+          leading: (backButton) ? BackButton() : null,
         );
 }
