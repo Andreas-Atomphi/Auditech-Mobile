d2c79719356de2adbd89009817208362ace9f8a7
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-08-29 06:20:12 -0300

    [FEAT] Criada a tela de instrução de treinamento.

---
+++ lib/telas/TelaTreinamentoInstrucoes.dart
@@ -0,0 +1,80 @@
+import 'package:AuditechMobile/main.dart';
+import 'package:AuditechMobile/telas/CustomComponents/Global/globalComponents.dart';
+import 'package:AuditechMobile/telas/CustomComponents/TelaTreinamento/components.dart';
+import 'package:AuditechMobile/telas/Telas.dart';
+import 'routes.dart';
+import 'package:flutter/material.dart';
+
+class _TreinamentoInstrucoesState extends State<TelaInstrucoesTreinamento> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: CAppBar("Treinamento[Num]"),
+        body: Column(
+          children: [
+            Spacer(
+              flex: 1,
+            ),
+            CardInstruct("texto"),
+            Row(
+              children: [
+                Spacer(
+                  flex: 1,
+                ),
+                Container(
+                  child: RaisedButton(
+                    child: Text("Voltar"),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),
+                Spacer(
+                  flex: 6,
+                ),
+                Container(
+                  child: RaisedButton(
+                    child: Text("Pular"),
+                    color: Colors.white,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: routes["treinamento"],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+                Container(
+                  child: RaisedButton(
+                    child: Text("Próxima"),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),
+                Spacer(
+                  flex: 1,
+                ),
+              ],
+            ),
+            Spacer(
+              flex: 1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TelaInstrucoesTreinamento extends StatefulWidget {
+  State<TelaInstrucoesTreinamento> createState() {
+    return _TreinamentoInstrucoesState();
+  }
+}
