8215c1390ad11ac54d6dbbb3bf153995150b8a72
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-02 18:21:11 -0300

    [FEAT] Adicionadas anotações e removidas os imports inúteis. [FEAT] debugShowCheckedModeBanner alterada para false.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/Telas.dart';
 import 'package:AuditechMobile/telas/routes.dart';
-import 'package:AuditechMobile/telas/CustomComponents/Global/globalComponents.dart';
 
 Color backgroundColor = Color.fromARGB(255, 0, 90, 100);
 Brightness theme = Brightness.light;
-Color primary = Color.fromARGB(255, 0, 200, 190);
-Color accent = Color.fromARGB(255, 0, 150, 160);
+Color primary = Color.fromARGB(255, 0, 200, 190); //Variável para a cor global
+Color accent = Color.fromARGB(255, 0, 150, 160); // Variável para cor secundária
 void main() => setAppup();
 MaterialApp aplicativo = MaterialApp(
+  debugShowCheckedModeBanner: false,
   initialRoute: "principal",
   routes: routes,
   home: TelaLogin(),
