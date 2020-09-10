b2b53578f9358fef9658c36bb6ba6dca87f59d4d
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-10 15:07:05 -0300

    [FEAT] Adicionados os dados e removido a placeholder InstructAndGreetings (WIP).

--- lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaEstatísticas.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaEstatísticas.dart
@@ -1,20 +1,50 @@
+import 'package:AuditechMobile/telas/CustomComponents/Global/Gr%C3%A1ficoPizza.dart';
+import 'package:AuditechMobile/telas/CustomComponents/Global/RGr%C3%A1fico.dart';
+import 'package:fl_animated_linechart/common/dates.dart';
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
 
 import '../components.dart';
 
 class AbaEstatisticas extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Spacer(
-          flex: 1,
-        ),
-        InstructAndGreetings(
-            "Aqui será mostrado as estatísticas do Usuário (se está progredindo, regredindo ou estagnado)."),
-        Spacer(
-          flex: 2,
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          GraficoPizza(
+            data: [
+              {
+                "rankkey": "issae",
+                "fatias": [
+                  {"n": 500, "cor": Colors.yellow[500], "chave": "q1"},
+                  {"n": 500, "cor": Colors.red[500], "chave": "q2"},
+                  {"n": 300, "cor": Colors.green[500], "chave": "q3"},
+                ],
+              },
+            ],
+          ),
+          RGrafico(
+            width: 300.0,
+            height: 300.0,
+            bgcolor: Colors.white,
+            lines: [
+              {
+                "nome": "fase",
+                "cor": Colors.red,
+                "pontos": [
+                  {"x": 0, "y": 0},
+                  {"x": 1, "y": 1},
+                  {"x": 2, "y": 4}
+                ]
+              },
+            ],
+            gradients: [
+              [Colors.blue, Colors.transparent],
+            ],
+            fromTo: Dates(DateTime(2000), DateTime(2001)),
+          )
+        ],
+      ),
     );
   }
 }
