1af61ec3f0dd09b64f17b1976803a1994755c8cc
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-10 15:03:37 -0300

    [FEAT] Removidos os imports inúteis. [FEAT] Adicionado gráfico cartesiano. [FEAT] Adicionado opções de tentar denovo e de sair do exercício.

--- lib/telas/TelaResultados.dart
+++ lib/telas/TelaResultados.dart
@@ -1,10 +1,6 @@
 import 'package:AuditechMobile/telas/CustomComponents/Global/globalComponents.dart';
-import 'package:fl_animated_linechart/chart/area_line_chart.dart';
-import 'package:fl_animated_linechart/chart/chart_line.dart';
-import 'package:fl_animated_linechart/chart/chart_point.dart';
+import 'package:AuditechMobile/telas/CustomComponents/TelaResultados/components.dart';
 import 'package:fl_animated_linechart/common/dates.dart';
-import 'package:fl_animated_linechart/fl_animated_linechart.dart';
-import 'package:fl_animated_linechart/common/pair.dart';
 import 'package:flutter/material.dart';
 
 class _SResultados extends State {
@@ -25,10 +21,64 @@ class _SResultados extends State {
                 Spacer(
                   flex: 1,
                 ),
+                RGrafico(
+                  width: ((MediaQuery.of(context).size.width - 10) > 300)
+                      ? 300
+                      : MediaQuery.of(context).size.width - 10,
+                  height: ((MediaQuery.of(context).size.width - 10) > 300)
+                      ? 300
+                      : MediaQuery.of(context).size.width,
+                  filled: false,
+                  fromTo: Dates(
+                    DateTime(1),
+                    DateTime(2),
+                  ),
+                  gradients: [
+                    [
+                      Colors.red[900],
+                      Color.fromRGBO(0, 100, 200, 0),
+                    ],
+                  ],
+                  lines: [
+                    {
+                      "nome": "Desempenho",
+                      "cor": Colors.blue,
+                      "pontos": [
+                        {"x": 0.0, "y": 0.0},
+                        {"x": 1.0, "y": 1.0},
+                        {"x": 2.0, "y": 2.0},
+                        {"x": 3.0, "y": 1.0},
+                        {"x": 4.0, "y": 2.0},
+                        {"x": 5.0, "y": 3.0},
+                        {"x": 6.0, "y": 4.0},
+                      ]
+                    },
+                  ],
+                ),
                 Spacer(
                   flex: 1,
                 ),
               ],
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pop(context);
+                Navigator.pop(context);
+              },
+              child: Text("Voltar a tela principal"),
+              color: Colors.grey[300],
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pop(context);
+              },
+              child: Text("Tentar de novo"),
+              color: Colors.grey[300],
             ),
             Spacer(
               flex: 1,
