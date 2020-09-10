ad67c1e7b430ca230c97da24ab72dce9e4b72204
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-10 15:01:07 -0300

    [FEAT] Rotas movidas de routes.dart para main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/Telas.dart';
-import 'package:AuditechMobile/telas/routes.dart';
 
 Color backgroundColor = Color.fromARGB(255, 0, 90, 100);
 Brightness theme = Brightness.light;
 Color primary = Color.fromARGB(255, 0, 200, 190); //Variável para a cor global
 Color accent = Color.fromARGB(255, 0, 150, 160); // Variável para cor secundária
+
+Map<String, Widget Function(BuildContext context)> routes = {
+  "principal": (context) => TelaLogin(),
+  "boas-vindas": (context) => TelaBoasVindas(),
+  "treinamento-aviso": (context) => TelaInstrucoesTreinamento(),
+  "treinamento-exemplo": (context) => TelaTreinamentoExemplo(),
+  "treinamento-3": (context) => Exercicio3(),
+  "resultados": (context) => Resultados(),
+};
+
 void main() => setAppup();
 MaterialApp aplicativo = MaterialApp(
   debugShowCheckedModeBanner: false,
-  initialRoute: "resultados",
+  initialRoute: "principal",
   routes: routes,
   home: TelaLogin(),
   theme: ThemeData(
