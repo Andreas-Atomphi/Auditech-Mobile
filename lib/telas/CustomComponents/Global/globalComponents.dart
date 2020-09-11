b2d5aaf5b6f8efb0bb0a9cb37a9dbf455ae24a2d
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-11 16:40:09 -0300

    [FEAT] Adicionados mais componentes globais.

--- lib/telas/CustomComponents/Global/globalComponents.dart
+++ lib/telas/CustomComponents/Global/globalComponents.dart
@@ -2,3 +2,7 @@ library global_components;
 
 export 'CAppBar.dart';
 export 'StringsLoader.dart';
+export 'GCartesiano.dart';
+export 'RGrafico.dart';
+export 'GraficoPizza.dart';
+export 'teste.dart';
