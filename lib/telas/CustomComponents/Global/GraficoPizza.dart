3e3f5bc9b5ff81d5798a686909154b00bab22944
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-11 16:43:15 -0300

    [FEAT] Retirado o acento do Gráfico. [FEAT ] Trocado o gráfico pizza de CircularChart para PieChart.

---
+++ lib/telas/CustomComponents/Global/GraficoPizza.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_circular_chart/flutter_circular_chart.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class GraficoPizza extends StatelessWidget {
+  final bool legenda, legendasLadoALado;
+  final LegendPosition posicaoLegenda;
+  final Map<String, double> dados;
+  final List<Color> cores;
+  final double width, height, radius;
+
+  GraficoPizza(
+    this.dados, {
+    this.legenda,
+    this.legendasLadoALado,
+    this.width,
+    this.height,
+    this.radius,
+    this.posicaoLegenda,
+    this.cores,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      child: PieChart(
+        chartRadius: radius,
+        legendOptions: LegendOptions(
+          legendPosition: LegendPosition.bottom,
+          showLegends: true,
+          showLegendsInRow: true,
+        ),
+        dataMap: dados,
+        colorList: cores,
+      ),
+    );
+  }
+}
