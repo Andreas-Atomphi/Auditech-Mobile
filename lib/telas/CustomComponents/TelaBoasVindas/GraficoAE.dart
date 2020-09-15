142a743896a8e5b0daeec10a70666350f0aa75ff
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-15 16:02:05 -0300

    [FEAT] Criado o componente GraficoAE.

---
+++ lib/telas/CustomComponents/TelaBoasVindas/GraficoAE.dart
@@ -0,0 +1,33 @@
+/* Os gráficos da Aba de estatísitca */
+
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class GraficoAE extends Container {
+  final double width, height;
+  final List<double> dados;
+  final List<Color> cores;
+  final String titulo;
+  GraficoAE({
+    this.width,
+    this.height,
+    this.dados,
+    this.cores,
+    this.titulo,
+  }) : super(
+          width: width,
+          height: height,
+          child: PieChart(
+            dataMap: {
+              "Acertos": dados[0],
+              "Erros": dados[1],
+              "Não respondeu": dados[2],
+            },
+            legendOptions: LegendOptions(
+              showLegends: false,
+            ),
+            colorList: cores,
+            centerText: titulo,
+          ),
+        );
+}
