02b041d55a6b0c08835d1b7a54d24ffccd020160
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-15 16:00:42 -0300

    [FEAT] Removido o conteúdo de Conjunct.dart.

--- lib/telas/CustomComponents/TelaBoasVindas/Conjunct.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/Conjunct.dart
@@ -1,5 +1,1 @@
-List<Map<String, dynamic>> ltwConjunct = [
-  {"texto": "Configurações", "metodo": () => null},
-  {"texto": "Sobre DPAC", "metodo": () => null},
-  {"texto": "Sair", "metodo": () => null},
-];
+
