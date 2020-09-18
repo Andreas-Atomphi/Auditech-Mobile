a5a1ba43850de74c24de33a19fea0be437a45d15
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-18 15:45:53 -0300

    [FEAT] Funções de estilo de texto movidos para a classe FormatoTexto.

--- lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaBoasVindas.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaBoasVindas.dart
@@ -5,63 +5,27 @@ import '../components.dart';
 
 class AbaBoasVindas extends StatelessWidget {
   Widget build(BuildContext context) {
-    Color corTexto = Colors.grey[800];
-    Color corD1 = Colors.red[800];
-
-    TextStyle title({FontWeight weight = FontWeight.normal}) => TextStyle(
-          color: Colors.grey[800],
-          fontSize: 40,
-          fontFamily: "OpenSans",
-          fontWeight: weight,
-        );
-
-    TextStyle subtitulo(
-            {Color cor = const Color(0xFF424242),
-            FontWeight weight = FontWeight.normal}) =>
-        TextStyle(
-            color: cor,
-            fontSize: 25,
-            fontFamily: "OpenSans",
-            fontWeight: weight);
-
-    TextStyle topico(
-            {Color cor = const Color(0xFF424242),
-            FontWeight weight = FontWeight.normal}) =>
-        TextStyle(
-            color: cor,
-            fontSize: 21,
-            fontFamily: "OpenSans",
-            fontWeight: weight);
-
-    TextStyle corpo(
-            {Color cor = const Color(0xFF424242),
-            FontWeight weight = FontWeight.normal}) =>
-        TextStyle(
-          color: cor,
-          fontSize: 16,
-          fontFamily: "OpenSans",
-          fontWeight: weight,
-        );
-
     return Column(
       children: [
         Spacer(
           flex: 1,
         ),
-        InstructAndGreetings(
-          [
+        ScrollableCardColumn(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height * 0.7,
+          children: [
             RichText(
               textAlign: TextAlign.center,
               text: TextSpan(
                 children: [
                   TextSpan(
                     text: "Bem-vindo!\n\n\n",
-                    style: title(weight: FontWeight.bold),
+                    style: FormatoTexto.title(weight: FontWeight.bold),
                   ),
                   TextSpan(
                     text:
                         "Olá, bem-vindo ao Auditech, o seu app de tratamento ao DPAC, Segue abaixo as instruções de como usar o app\n\n",
-                    style: corpo(),
+                    style: FormatoTexto.corpo(),
                   ),
                 ],
               ),
@@ -72,34 +36,51 @@ class AbaBoasVindas extends StatelessWidget {
                 children: [
                   TextSpan(
                     text: "Estatísticas\n",
-                    style: subtitulo(weight: FontWeight.bold),
+                    style: FormatoTexto.subtitulo(weight: FontWeight.bold),
                     children: [
-                      TextSpan(
-                        text:
-                            "Nessa aba fica registrado sua pontuação, e te informa se você está progredindo, regredindo ou parado. Esta aba é dividida em duas seções, sendo:\n\n",
-                        style: corpo(),
+                      WidgetSpan(
+                        child: SizedBox(
+                          child: Text(
+                            "Nessa aba fica registrado sua pontuação, e te informa se você está progredindo, regredindo ou parado. Esta aba é dividida em duas seções, sendo:\n",
+                            style: FormatoTexto.corpo(),
+                          ),
+                        ),
                       ),
                       TextSpan(
                         text: "Estatística global\n",
-                        style: topico(weight: FontWeight.bold),
+                        style: FormatoTexto.topico(weight: FontWeight.bold),
                         children: [
                           TextSpan(
                             text:
                                 "Aqui fica a conclusão das suas pontuações nos exercícios, a partir da conclusão é informado se você está progredindo, regredindo ou parado.\n\n",
-                            style: corpo(),
+                            style: FormatoTexto.corpo(),
                           ),
                         ],
                       ),
                       TextSpan(
                         text: "Estatísticas das fases\n",
-                        style: topico(weight: FontWeight.bold),
+                        style: FormatoTexto.topico(weight: FontWeight.bold),
                         children: [
                           TextSpan(
                             text:
                                 "Aqui fica a conclusão das suas pontuações nos exercícios, a partir da conclusão é informado se você está progredindo, regredindo ou parado.\n\n",
-                            style: corpo(),
+                            style: FormatoTexto.corpo(),
                           ),
                         ],
+                      ),
+                    ],
+                  ),
+                  TextSpan(
+                    text: "Treinamentos\n",
+                    style: FormatoTexto.subtitulo(weight: FontWeight.bold),
+                    children: [
+                      WidgetSpan(
+                        child: SizedBox(
+                          child: Text(
+                            "Nessa aba ficam seus treinos, você poderá acessar apenas os que estiverem liberados para você, quem libera é o seu clínico apenas depois de ver que você está em progresso, então... mãos na massa.\n",
+                            style: FormatoTexto.corpo(),
+                          ),
+                        ),
                       ),
                     ],
                   ),
