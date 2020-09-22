7ed0607a2aa1fc53fee8141e33dc4dba23f7009c
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-22 16:23:50 -0300

    [FEAT] Reformulação da classe Playback.

--- lib/telas/CustomComponents/TelaTreinamento/Playback.dart
+++ lib/telas/CustomComponents/TelaTreinamento/Playback.dart
@@ -1,19 +1,19 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 // import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/material.dart';
 
 final List<String> examples = <String>[
   "assets/audios/file_example_MP3_2MG.mp3",
 ]; // Áudios da pasta exemplos
 
 final List<String> exercicios = <String>[
-  "audios/Exercicios/10_sequencia_animais.mp3",
-  "audios/Exercicios/11_sequencia_instrumentos.mp3",
-  "audios/Exercicios/12_sequencia_natureza.mp3",
-  "audios/Exercicios/13_corpo_humano.mp3",
-  "audios/Exercicios/14_sequencia_transporte.mp3",
-  "audios/Exercicios/15_sequencia_casa.mp3",
+  "assets/audios/Exercicios/10_sequencia_animais.mp3",
+  "assets/audios/Exercicios/11_sequencia_instrumentos.mp3",
+  "assets/audios/Exercicios/12_sequencia_natureza.mp3",
+  "assets/audios/Exercicios/13_corpo_humano.mp3",
+  "assets/audios/Exercicios/14_sequencia_transporte.mp3",
+  "assets/audios/Exercicios/15_sequencia_casa.mp3",
 ];
 
 // final List<Audio> audios = examples.map((e) => Audio(e)).toList();
@@ -23,7 +23,7 @@ enum Players {
   AC,
 }
 
-extension PlayersFullName on Players {
+extension PlayersComponents on Players {
   dynamic get playerComponent {
     switch (this) {
       case Players.AAP:
@@ -38,48 +38,15 @@ extension PlayersFullName on Players {
   }
 }
 
-class Playback extends StatelessWidget {
-  final Players player;
-
-  Playback(this.player);
-
-  void action(String path, [what = "play"]) {
-    var selectedp = player.playerComponent;
-    Map<bool, void Function()> playOptions = {
-      player == Players.AAP: () async {
-        try {
-          if (what == "play") {
-            await selectedp.open(
-              Audio(path),
-              autoStart: true,
-            );
-          } else if (what == "stop") {
-            selectedp.stop();
-          }
-        } catch (e) {
-          print(e);
-        }
-      },
-      player == Players.AC: () async {
-        try {
-          if (what == "play") {
-            final result = await selectedp.play(
-              path,
-            );
-          } else if (what == "stop") {
-            selectedp.stop();
-          }
-          print(path);
-        } catch (e) {
-          print(e);
-        }
-      }
-    };
-    playOptions[true]();
+class Playback {
+  final AudioPlayer audioPlayer = new AudioPlayer();
+  Playback();
+  play(String path) async {
+    int result = await audioPlayer.play(path, isLocal: true);
+    return result;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return this;
+  stop() async {
+    audioPlayer.stop();
   }
 }
