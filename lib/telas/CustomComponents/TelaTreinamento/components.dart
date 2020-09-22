2b5aab37d56a90d462f9cd0417f1245e74450d11
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-09-22 16:23:06 -0300

    [FEAT] Editados 3 exports de components.dart.

--- lib/telas/CustomComponents/TelaTreinamento/components.dart
+++ lib/telas/CustomComponents/TelaTreinamento/components.dart
@@ -1,5 +1,5 @@
 export 'Playback.dart';
 export 'Instruções/CardInstruct.dart';
-export 'ComponentesDeExercício/ButtonSide.dart';
-export 'ComponentesDeExercício/ButtonSelect.dart';
-export 'ComponentesDeExercício/TecladoNumerico.dart';
+export 'ComponentesDeExercicio/ButtonSide.dart';
+export 'ComponentesDeExercicio/ButtonSelect.dart';
+export 'ComponentesDeExercicio/TecladoNumerico.dart';
