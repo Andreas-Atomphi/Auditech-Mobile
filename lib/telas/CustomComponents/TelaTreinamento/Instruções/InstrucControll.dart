b7b1aa91871ad44a2659c9cd58daacc3556f0bd7
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-13 17:13:30 -0300

    [FEAT] Criado o componente InstrucControll.

---
+++ lib/telas/CustomComponents/TelaTreinamento/Instruções/InstrucControll.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:AuditechMobile/mainData.dart';
+
+class InstrucControll extends StatelessWidget {
+  final Widget visor;
+  final Color btnColors, bgColor, textColor;
+
+  InstrucControll(this.visor, this.btnColors, this.bgColor, this.textColor);
+
+  Widget build(BuildContext context) {
+    return Container(
+      color: bgColor,
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          visor,
+          Row(
+            children: [
+              Spacer(
+                flex: 1,
+              ),
+              Container(
+                child: RaisedButton(
+                  child: Icon(
+                    Icons.arrow_back_ios_sharp,
+                    color: textColor,
+                  ),
+                  color: btnColors,
+                  onPressed: () {},
+                  elevation: 0.0,
+                ),
+              ),
+              Spacer(
+                flex: 6,
+              ),
+              Container(
+                child: RaisedButton(
+                  child: Icon(
+                    Icons.arrow_forward_ios_sharp,
+                    color: Colors.white,
+                  ),
+                  color: corDeDestaque,
+                  onPressed: () {},
+                  elevation: 0.0,
+                ),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
