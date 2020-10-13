8e6bec8cdf90e6f15df78b2599afa9718c75f2a8
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-13 17:11:47 -0300

    [FEAT] Criado o componente InstrucControll.

--- lib/telas/CustomComponents/TelaTreinamento/components.dart
+++ lib/telas/CustomComponents/TelaTreinamento/components.dart
@@ -1,5 +1,6 @@
 export 'Playback.dart';
 export 'Instruções/CardInstruct.dart';
+export 'Instruções/InstrucControll.dart';
 export 'ComponentesDeExercicio/ButtonSide.dart';
 export 'ComponentesDeExercicio/ButtonSelect.dart';
 export 'ComponentesDeExercicio/TecladoNumerico.dart';
