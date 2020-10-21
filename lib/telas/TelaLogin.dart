518817e8c1469d1bc1f7fa127c9505d35e076d10
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-21 19:55:33 -0300

    [FEAT] Adicionados algorítmos para Formatar CPF e Data. [FEAT] Iniciada a implementação de request para Login.

--- lib/telas/TelaLogin.dart
+++ lib/telas/TelaLogin.dart
@@ -1,11 +1,62 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:AuditechMobile/telas/CustomComponents/Global/globalComponents.dart';
 import 'package:flutter/material.dart';
 import 'package:AuditechMobile/telas/CustomComponents/TelaLogin/components.dart';
 import 'package:AuditechMobile/mainData.dart';
+import 'package:http/http.dart' as http;
+import 'package:sprintf/sprintf.dart';
 
 class TelaLogin extends StatelessWidget {
   Widget build(BuildContext context) {
-    void entrar() {
+    List<TextFieldLogin> loginFields = [
+      TextFieldLogin("CPF", false, TipoEntrada.CPF),
+      TextFieldLogin("Data de Aniversário", false, TipoEntrada.DT),
+    ];
+
+    String cpfFormat(String str) {
+      if (str.length < 11) return null;
+      String one = str.substring(0, 3);
+      String two = str.substring(3, 6);
+      String three = str.substring(6, 9);
+      String four = str.substring(9, 11);
+
+      return "$one.$two.$three-$four";
+    }
+
+    String dtFormat(String str) {
+      if (str.length < 10) return null;
+      String one = str.substring(0, 2);
+      String two = str.substring(2, 4);
+      String three = str.substring(4, 8);
+      return "$one/$two/$three 00:00:00";
+    }
+
+    Future entrar() async {
+      /*
+      //Formata os valores da TextField
+      String cpf = cpfFormat(loginFields[0].text);
+      String dtNasc = dtFormat(loginFields[1].text);
+
+      const Map<String, String> corpo = <String, String>{
+        "cpf": "123.456.789-00",
+        "dataNascimento": "01/01/1990 00:00:00"
+      };
+
+      //Define a url e corpo para enviar
+      final Uri auditechAPI = Uri.parse(
+          "http://hawgamtech.somee.com/AuditechAPI/usuarios/ValidarUsuario");
+      final uri = auditechAPI.replace(queryParameters: corpo);
+
+      //Checa se existe
+      http.Response existe = await http.get(
+        uri,
+        headers: {
+          HttpHeaders.contentTypeHeader: 'application/json',
+        },
+      );
+      print(existe.body);*/
       Navigator.push(
         context,
         MaterialPageRoute(builder: routes["boas-vindas"]),
@@ -19,9 +70,9 @@ class TelaLogin extends StatelessWidget {
         body: FormLogin(
           [
             Spacer(flex: 8),
-            TextFieldLogin("Email", false),
+            loginFields[0],
             Spacer(flex: 1),
-            TextFieldLogin("Senha", true),
+            loginFields[1],
             Spacer(flex: 1),
             ButtonLogin("Entrar", () => entrar(), false),
             ButtonLogin("Registrar-se", () {}, true),
