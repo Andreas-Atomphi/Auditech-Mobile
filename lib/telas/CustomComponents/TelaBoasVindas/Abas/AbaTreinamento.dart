9e935c927db5abbc91786646271424399c294228
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-22 16:15:05 -0300

    [FEAT] Lista de componentes adicionada para modificações.

--- lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaTreinamento.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaTreinamento.dart
@@ -17,27 +17,29 @@ class AbaTreinamento extends StatelessWidget {
     }
 
     List model = ["Exercício ", "treinamento-"];
+    List<Widget> buttons = [
+      //Adiciona os componentes de forma dinâmica através das chaves de routesExercicios
+      ...routesExercicios.keys.map(
+        //Retorna ButtonTreinamento
+        (e) => ButtonTreinamento(
+          //Concatena model com o último dígito da chave (treinamento-n)
+          model[0] + e[e.length - 1],
+          //Adiciona o método para ButtonTreinamento
+          () => irParaTreino(
+            model[0] + e[e.length - 1],
+            e,
+          ),
+        ),
+      ),
+    ];
+
     double space = 30;
     return GridView.extent(
       padding: EdgeInsets.all(space),
       maxCrossAxisExtent: 200,
       crossAxisSpacing: space,
       mainAxisSpacing: space,
-      children: [
-        //Adiciona os componentes de forma dinâmica através das chaves de routesExercicios
-        ...routesExercicios.keys.map(
-          //Retorna ButtonTreinamento
-          (e) => ButtonTreinamento(
-            //Concatena model com o último dígito da chave (treinamento-n)
-            model[0] + e[e.length - 1],
-            //Adiciona o método para ButtonTreinamento
-            () => irParaTreino(
-              model[0] + e[e.length - 1],
-              e,
-            ),
-          ),
-        )
-      ],
+      children: [...buttons],
     );
   }
 }
