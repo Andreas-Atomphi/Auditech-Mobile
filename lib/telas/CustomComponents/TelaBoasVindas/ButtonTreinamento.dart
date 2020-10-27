1f98fec743197d0c6f6ded570396750bcb5d0855
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-27 16:08:07 -0300

    [FEAT] Esquema de desativação de ButtonTreinamento adicionado.

--- lib/telas/CustomComponents/TelaBoasVindas/ButtonTreinamento.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/ButtonTreinamento.dart
@@ -1,30 +1,38 @@
+import 'package:AuditechMobile/mainData.dart';
 import 'package:flutter/material.dart';
 
 class _SButtonTreinamento extends State<ButtonTreinamento> {
   String texto;
   void Function() aoPressionar;
-  bool ativado = true;
+  bool _ativado;
 
   @override
   void initState() {
     super.initState();
     aoPressionar = widget._aoPressionar;
     texto = widget._texto;
+    _ativado = widget._ativado;
+  }
+
+  set ativado(bool a) {
+    _ativado = a;
   }
 
   Widget build(BuildContext context) {
     return Container(
-      child: FlatButton(
-        onPressed: (ativado) ? aoPressionar : null,
+      child: RaisedButton(
+        onPressed: (_ativado) ? aoPressionar : null,
         child: Text(
           texto,
           style: TextStyle(
-            color: Colors.white,
+            color: (_ativado && aoPressionar != null)
+                ? Colors.white
+                : Colors.blueGrey[500],
             fontWeight: FontWeight.w600,
             fontSize: 17,
           ),
         ),
-        color: Colors.orange,
+        color: (_ativado && aoPressionar != null) ? corDeDestaque : secondary,
       ),
     );
   }
@@ -33,10 +41,27 @@ class _SButtonTreinamento extends State<ButtonTreinamento> {
 class ButtonTreinamento extends StatefulWidget {
   final String _texto;
   final void Function() _aoPressionar;
-
-  ButtonTreinamento(this._texto, this._aoPressionar);
+  final bool _ativado;
+  ButtonTreinamento(
+    this._texto,
+    this._aoPressionar, [
+    this._ativado = false,
+  ]);
+  final _state = _SButtonTreinamento();
 
   State<ButtonTreinamento> createState() {
-    return _SButtonTreinamento();
+    return _state;
+  }
+
+  void ativar() {
+    _state.ativado = true;
+  }
+
+  void desativar() {
+    _state.ativado = false;
+  }
+
+  bool get estaAtivado {
+    return _ativado;
   }
 }
