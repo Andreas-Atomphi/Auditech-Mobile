4bc10c1bb431210c6bdda119d1c0a4203f719f8a
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-28 16:48:16 -0300

    [FEAT] AbaTreinamento agora só te deixa acessar os exercicios existentes e que a fase permite.

--- lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaTreinamento.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/Abas/AbaTreinamento.dart
@@ -1,10 +1,14 @@
 import 'package:AuditechMobile/telas/Telas.dart';
 import 'package:flutter/material.dart';
-
+import 'package:intl/intl.dart';
 import 'package:AuditechMobile/mainData.dart';
 import '../components.dart';
 
 class AbaTreinamento extends StatelessWidget {
+  final Map fase;
+
+  AbaTreinamento({this.fase});
+
   Widget build(BuildContext context) {
     void irParaTreino(String appbartext, [String numtreino = "exemplo-tr"]) {
       Navigator.push(
@@ -17,14 +21,32 @@ class AbaTreinamento extends StatelessWidget {
     }
 
     List model = ["Exercício ", "treinamento-"];
+    DateFormat dataPadrao = DateFormat("dd/MM/yyyy HH:mm:ss");
+    Map<String, DateTime> data = {
+      'inicio': dataPadrao.parse(fase['dataInicio']),
+      'fim': dataPadrao.parse(fase['dataFinal']),
+      'atual': DateTime.now(),
+    };
+
+    print(data);
+    print(
+      data['atual'].compareTo(
+        data['inicio'],
+      ),
+    );
+    print(
+      data['atual'].compareTo(
+        data['fim'],
+      ),
+    );
     List<ButtonTreinamento> buttons = [
       //Adiciona os componentes de forma dinâmica através das chaves de routesExercicios
       ...routesExercicios.keys.map(
         //Retorna ButtonTreinamento
         (e) {
+          int exercicioNum = int.parse(e.replaceAll(RegExp('[^0-9]'), ''));
           //  Armazena os números dos treinos
-          String exercicio =
-              "${model[0]} ${int.parse(e.replaceAll(RegExp('[^0-9]'), ''))}";
+          String exercicio = "${model[0]} $exercicioNum";
           //  Retorna um ButtonTreinamento
           return ButtonTreinamento(
             //Concatena model com o último dígito da chave (treinamento-n)
@@ -34,6 +56,16 @@ class AbaTreinamento extends StatelessWidget {
               exercicio,
               e,
             ),
+            ((fase != null) &&
+                (fase['exercicioIdExercicio'] == exercicioNum) &&
+                (data['atual'].compareTo(
+                          data['inicio'],
+                        ) >=
+                        0 &&
+                    data['atual'].compareTo(
+                          data['fim'],
+                        ) <=
+                        0)),
           );
         },
       ),
