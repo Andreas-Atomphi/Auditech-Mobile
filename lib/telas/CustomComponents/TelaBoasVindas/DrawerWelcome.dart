217ddb9b22debee2b1cc4bb35b32dd748adcdba8
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-10-30 23:58:42 -0300

    [FEAT] Ícone do Drawer posicionado.

--- lib/telas/CustomComponents/TelaBoasVindas/DrawerWelcome.dart
+++ lib/telas/CustomComponents/TelaBoasVindas/DrawerWelcome.dart
@@ -7,7 +7,7 @@ class DrawerWelcome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: Ink(
-        color: Color.fromARGB(255, 0, 150, 140),
+        color: Color.fromRGBO(0, 150, 140, 1),
         child: ListView(
           children: <Widget>[
             Stack(
@@ -16,35 +16,47 @@ class DrawerWelcome extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Colors.blue,
                     image: DecorationImage(
-                        image: AssetImage(
-                          'assets/images/Logo_02.jpg',
-                        ),
-                        fit: BoxFit.cover),
+                      image: AssetImage(
+                        'assets/images/Logo_02.jpg',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                   child: null,
                 ),
-                Container(
-                  padding: EdgeInsets.all(2),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: Colors.white,
-                  ),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                    radius: 45,
-                    child: Text(
-                      "U",
-                      style: TextStyle(fontSize: 45),
+                Positioned(
+                  left: 5,
+                  child: Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.white,
                     ),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      radius: 45,
+                      child: Text(
+                        "U",
+                        style: TextStyle(
+                          fontSize: 45,
+                          fontFamily: "OpenSans",
+                        ),
+                      ),
+                    ),
+                    margin: EdgeInsets.only(top: 60),
                   ),
-                  margin: EdgeInsets.only(top: 60),
                 )
               ],
             ),
-            ...mapDados.map((e) {
-              return ListTileWelcome(e["texto"], e["metodo"]);
-            }).toList(),
+            ...mapDados
+                .map(
+                  (e) => ListTileWelcome(
+                    e["texto"],
+                    e["metodo"],
+                  ),
+                )
+                .toList(),
           ],
         ),
       ),
