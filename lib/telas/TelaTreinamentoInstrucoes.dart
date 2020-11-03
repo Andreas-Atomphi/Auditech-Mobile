8e95054c32b795735fe65b4e0295785f0ab8b2d8
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-11-03 17:57:40 -0300

    [FEAT] Sistema de formatação completo.

--- lib/telas/TelaTreinamentoInstrucoes.dart
+++ lib/telas/TelaTreinamentoInstrucoes.dart
@@ -41,88 +41,33 @@ class _TreinamentoInstrucoesState extends State<TelaInstrucoesTreinamento> {
     }
 
     TextSpan formatRules(String str) {
-      final reg = RegExp(
-        r"^\*[a-zA-Z]\{.*[a-zA-Z]\:.*\,.*\w*(text)\:.*[^\n]\}[a-zA-Z]\*",
-        multiLine: true,
-        caseSensitive: true,
+      print(str);
+      String text = str.substring(
+        str.indexOf(RegExp(r'\".*\"')) + 1,
+        str.indexOf(RegExp(r'\"\}')),
       );
-
-      final propertyReg = RegExp(r"\:.*[0-9]\,");
-      final textReg = RegExp(r'\".*\"');
-      final boldReg = RegExp(r'\*b\{.*\}b\*');
-
-      String prop = propertyReg.stringMatch("$str");
-      String tex = textReg.stringMatch("$str");
-      prop = prop.substring(
-        2,
-        prop.indexOf(RegExp(r"\,")),
+      String size = str.substring(
+        str.indexOf(RegExp(r'\:\s.*[0-9]')) + 1,
+        str.indexOf(RegExp(r'[0-9]\,')) + 1,
       );
-      String bolds;
-      tex = tex.substring(1, tex.length - 1);
-      //tex.substring(tex.indexOf(), );
-
-      print(tex);
-
-      for (int i = 0; i < 5; i++) {
-        final String absReg = r'\*b\{' + '$i';
-        final String reg = r'\*b\{' + '$i';
-        final String reg2 = r'' + '$i' + r'\}b\*';
-        final RegExp boldReg2 = RegExp(reg);
-        final RegExp boldReg3 = RegExp(reg2);
-        final RegExp absRegExp = RegExp(
-          absReg,
-          multiLine: true,
-          caseSensitive: true,
-        );
-        var left = tex.indexOf(
-          boldReg2,
-        );
-        var right = tex.indexOf(
-          boldReg3,
-        );
-        print(left);
-        print(right);
-        print(boldReg2);
-        print(boldReg3);
-
-        bolds = tex;
-        if (left >= 0 && right >= 0) {
-          print(
-            bolds.substring(
-              bolds.indexOf(
-                    boldReg2,
-                  ) +
-                  4,
-              bolds.indexOf(
-                boldReg3,
-              ),
-            ),
-          );
-          print(bolds.indexOf(
-            absRegExp,
-          ));
-          bolds = bolds.replaceAll(
-            absRegExp,
-            bolds.substring(
-              bolds.indexOf(
-                    boldReg2,
-                  ) +
-                  4,
-              bolds.indexOf(
-                boldReg3,
-              ),
-            ),
-          );
-        }
-      }
-
-      print(bolds);
+      List<String> texts;
+      texts = text.split(
+        RegExp(r'\\n'),
+      );
+      print(text);
+      print(texts);
+      print(size);
 
       return TextSpan(
-          style: TextStyle(
-            fontSize: double.parse(prop),
-          ),
-          text: tex);
+        style: TextStyle(
+          fontSize: double.parse(size),
+          color: Colors.black,
+          fontFamily: 'OpenSans',
+        ),
+        children: [
+          ...texts.map((e) => TextSpan(text: e + '\n\n')),
+        ],
+      );
     }
 
     formatRules("${widget.fase['exercicio']['descricaoExercicio']}");
@@ -148,15 +93,14 @@ class _TreinamentoInstrucoesState extends State<TelaInstrucoesTreinamento> {
                     width: MediaQuery.of(context).size.width,
                     height: MediaQuery.of(context).size.height * 0.6,
                     children: [
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.1,
+                      ),
                       RichText(
                         text: WidgetSpan(
                           child: RichText(
-                            text: TextSpan(
-                              text:
-                                  "${widget.fase['exercicio']['descricaoExercicio']}",
-                              style:
-                                  FormatoTexto.corpo(weight: FontWeight.bold),
-                            ),
+                            text: formatRules(
+                                "${widget.fase['exercicio']['descricaoExercicio']}"),
                           ),
                         ),
                       ),
