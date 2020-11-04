3767487f87e206d7cc3f8e6e88d8852cc7bb8e60
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-11-04 15:50:28 -0300

    [FEAT] App agora só fica em modo retrato.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:AuditechMobile/telas/TelaLogin.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'mainData.dart';
 
@@ -6,19 +7,27 @@ void main() {
   setAppup();
 }
 
-MaterialApp aplicativo = MaterialApp(
-  debugShowCheckedModeBanner: false,
-  initialRoute: "principal",
-  routes: routes,
-  home: TelaLogin(),
-  theme: ThemeData(
-    brightness: theme,
-    primaryColor: primary,
-    accentColor: secondary,
-    backgroundColor: backgroundColor,
-  ),
-);
+class MainApp extends StatelessWidget with PortraitModeMixin {
+  const MainApp();
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "principal",
+      routes: routes,
+      home: TelaLogin(),
+      theme: ThemeData(
+        brightness: theme,
+        primaryColor: primary,
+        accentColor: secondary,
+        backgroundColor: backgroundColor,
+      ),
+    );
+  }
+}
 
 void setAppup() {
-  runApp(aplicativo);
+  runApp(MainApp());
 }
